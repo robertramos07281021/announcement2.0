56f70b61513cbb6174f3bfb5d2b78f24ef0b72c5
robert dela cruz ramos <126318863+robertramos07281021@users.noreply.github.com>
2026-03-13 14:25:45 +0800

    new 1

---
+++ dbConnection/_db.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import "dotenv/config.js";
+
+
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      maxPoolSize: 20,
+      minPoolSize: 5,
+      socketTimeoutMS: 10 * 60 * 1000, // 10 minutes
+      serverSelectionTimeoutMS: 10 * 1000, // 10 seconds
+      connectTimeoutMS: 10 * 1000,
+    });
+    console.log("✅ MongoDB Connected...");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Error:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
